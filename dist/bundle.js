f79a92a6d49b030a3337a9535a51128eed7eb7f2
Tommy Mapp <tommy.mapp@hotmail.com>
2022-02-03 13:36:40 +0000

    Set up webpack

---
+++ dist/bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("This is a test!");
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsK0IiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly93ZWF0aGVyLWFwcC8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZyhcIlRoaXMgaXMgYSB0ZXN0IVwiKTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
