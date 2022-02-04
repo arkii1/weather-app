5865cb8cc543367ee095f9f72cc804504fbc75c3
Tommy Mapp <tommy.mapp@hotmail.com>
2022-02-04 13:30:11 +0000

    Set mode cors on fetch call

--- src/handleWeatherData.js
+++ src/handleWeatherData.js
@@ -3,7 +3,9 @@ const weatherId = '2645d0625b71eb9c2a11a1cf94cd794a';
 export default async function getWeather(location, callback) {
     try {
         const url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${weatherId}`;
-        const promise = await fetch(url);
+        const promise = await fetch(url, {
+            mode: 'cors',
+        });
         const data = await promise.json();
         callback(data);
     } catch (err) {
