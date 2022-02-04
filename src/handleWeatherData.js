f999daf505bafef52f6b13dfec12fd45df24183d
Tommy Mapp <tommy.mapp@hotmail.com>
2022-02-04 14:53:24 +0000

    Changed http to https

--- src/handleWeatherData.js
+++ src/handleWeatherData.js
@@ -2,7 +2,7 @@ const weatherId = '2645d0625b71eb9c2a11a1cf94cd794a';
 
 export default async function getWeather(location, callback) {
     try {
-        const url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${weatherId}&units=metric`;
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${weatherId}&units=metric`;
         const promise = await fetch(url, {
             mode: 'cors',
         });
